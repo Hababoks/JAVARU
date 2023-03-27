19c3da7ed5eb413f05719bca44b54b4b867dda17
Hababoks <126653941+Hababoks@users.noreply.github.com>
2023-03-27 14:04:28 +0300

    Add files via upload
    check small tasks to JavaRush

---
+++ GeneratorPozitivus/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Маринелло";
+        String date = "сегодня";
+        String time = "утром";
+        String noun = "генератор";
+        String adjective = "позитивный";
+        String verb = "выдает";
+        System.out.println("Новости:");
+        System.out.println(name + " " + date + " " + time + " изобрел " + adjective + " " + noun + " который " + verb + " террабайты данных в минуту.");
+    }
+}
+
+
---
+++ announcement/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+      String word = " World ";
+      String phrase = " will be better ";
+      String line = " if everyone ";
+      String text = " is clear.";
+        System.out.println(word + " " + phrase + " " + line + " " + text + " ");
+    }
+}
